8c98219cd77d2b0267c406d3de1000ca94d1ebd2
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-05-15 15:10:17 -0500

    fixed challenge 3

--- challenge003.rb
+++ challenge003.rb
@@ -11,7 +11,7 @@ pst_amount = sub_total * PST_RATE
 grand_total = sub_total + gst_amount + pst_amount
 
 puts "Subtotal: $#{sub_total }"
-puts "PST: $#{'%.2f' % pst_amount} - 7%"
+puts "PST: $#{ '%.2f' %pst_amount} - 7%"
 puts "GST: $#{gst_amount} - 5%"
 puts "Grand Total: $#{grand_total}"
 if(grand_total <= 5.00)
